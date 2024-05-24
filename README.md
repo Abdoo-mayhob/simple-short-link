003c1053d788ebf5fcf4210d244186e269dbce2b
a.almayhob <a.almayhob@haykalmedia.com>
2024-05-24 23:45:57 +0300

    Initial Release

---
+++ README.md
@@ -0,0 +1,37 @@
+# Simple Short Link
+
+**Contributors:** abdoo-mayhob  
+**Tags:** shortlink,post,short link,shorturl,url,link
+**Requires at least:** 5.0  
+**Tested up to:** 6.5.3  
+**Requires PHP:** 7.0  
+**Stable tag:** 1.0.0
+**License:** GPLv2 or later
+**License URI:** [https://www.gnu.org/licenses/gpl-2.0.html](https://www.gnu.org/licenses/gpl-2.0.html)    
+
+## Description
+
+Wordpress Plugin to Calculate and Show Estimated Reading Time in your posts in Both Arabic and English.
+With no perfomance Impact, and no external loaded styles or scripts
+
+## Installation
+
+1. Upload the `simple-short-link` folder to the `/wp-content/plugins/` directory.
+2. Activate the plugin through the 'Plugins' menu in WordPress.
+3. Enjoy !
+
+## Changelog
+### 1.0
+
+- Initial release
+
+## Frequently Asked Questions
+
+1. **Dose this plugin Use external shortlink providers ?**
+   - No, all operations are supported and inside the wordpress itself.
+
+2. **Dose this plugin creates additional tables or uses the database at all ?**
+   - No, This plugin will not make any read/write operations to the database at all, thus.. simple.
+
+# Credit
+- [Abdoo Mayhob](https://abdoo.me)
---
+++ simple-short-link.php
@@ -0,0 +1,76 @@
+<?php
+
+/**
+ *
+ * Plugin Name: Simple Short Link
+ * Plugin URI: https://github.com/Abdoo-mayhob/Estimated-Reading-Time
+ * Description: Makes short links to your posts without any external services or database bloat.
+ * Version: 1.0.0
+ * Author: Abdoo
+ * Author URI: https://abdoo.me
+ * License: GPLv2 or later
+ * Text Domain: simple-short-link
+ * Domain Path: /languages
+ *
+ * ===================================================================
+ * 
+ * Copyright 2024  Abdullatif Al-Mayhob, Abdoo abdoo.mayhob@gmail.com
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 3, as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * ===================================================================
+ * 
+ * TODO:
+ * - Add support for CPTs
+ */
+
+// If this file is called directly, abort.
+defined('ABSPATH') or die;
+
+// Load Translation Files, Only needed in admin area.
+add_action('admin_init', function () {
+    load_plugin_textdomain('simple-short-link', false, dirname(plugin_basename(__FILE__)) . '/languages');
+});
+
+// --------------------------------------------------------------------------------------
+// Shortlink Box
+
+add_action('add_meta_boxes', function() {
+    add_meta_box(
+        'shortlink-box', 
+        __('Short Link', 'simple-short-link'), 
+        'simple_shortlink_meta_box_cb', 
+        'post', 
+        'side', 
+        'high'
+    );
+});
+
+function simple_shortlink_meta_box_cb($post) {
+
+    // Short Link Works only on published posts.
+    if('publish' !== $post->post_status){
+        echo __('Short Link is for Published Posts Only.', 'simple-short-link');
+        return;
+    }
+    $post_link = site_url() . '/?p=' . $post->ID;
+    ?>
+    <input type="text" value="<?= $post_link ?>" id="shortlink" readonly>
+    <button type="button" class="button" onclick="CopyShortLink()"><?php _e('Copy', 'simple-short-link')?></button>
+    <script>
+        function CopyShortLink() {
+            var copyText = document.getElementById("shortlink");
+            copyText.select();
+            copyText.setSelectionRange(0, 99999);
+            document.execCommand("copy");
+        }
+    </script>
+    <?php
+}
